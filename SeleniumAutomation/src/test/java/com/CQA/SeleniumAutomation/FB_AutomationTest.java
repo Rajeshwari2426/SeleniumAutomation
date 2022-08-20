96bb7851a7211959313e7fbae26cc7dee1aec8b6
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-08-20 13:33:26 +0530

    [Rajeshwari] adding sign up test

--- SeleniumAutomation/src/test/java/com/CQA/SeleniumAutomation/FB_AutomationTest.java
+++ SeleniumAutomation/src/test/java/com/CQA/SeleniumAutomation/FB_AutomationTest.java
@@ -2,16 +2,22 @@ package com.CQA.SeleniumAutomation;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class FB_AutomationTest {
+	public static WebDriver driver;
+	@BeforeTest
+	public void launchBrowser() {
+		System.setProperty("webdriver.chrome.driver",
+			"C:\\Users\\rajar\\eclipse-workspace\\SeleniumAutomation\\SeleniumAutomation\\Drivers\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.get("https://www.facebook.com");		
+	}
+	
 	@Test
 	public void fbAutomationTest() throws InterruptedException
-	{
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\rajar\\eclipse-workspace\\SeleniumAutomation\\SeleniumAutomation\\Drivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com");
+	{					
 		driver.findElement(By.name("email")).sendKeys("Raji@gmail.com");
 		driver.findElement(By.id("pass")).sendKeys("Raji");
 		Thread.sleep(2000);
@@ -19,18 +25,18 @@ public class FB_AutomationTest {
 		
 	}
 	@Test
-	public void signupPageTest()
+	public void signupPageTest() throws InterruptedException
 	{
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\rajar\\eclipse-workspace\\SeleniumAutomation\\SeleniumAutomation\\Drivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com");
 		driver.findElement(By.linkText("Create New Account")).click();
+		Thread.sleep(3000);
 		driver.findElement(By.name("firstname")).sendKeys("Raji");
-//		driver.findElement(By.name("lastname")).sendKeys("gandi");
-//		driver.findElement(By.name("reg_email__")).sendKeys("Raji@gmail.com");
-//		driver.findElement(By.name("reg_passwd__")).sendKeys("Raji@gmail.com");
-//		
+		
+		driver.findElement(By.name("lastname")).sendKeys("gandi");
+		driver.findElement(By.name("reg_email__")).sendKeys("Raji@gmail.com");
+		driver.findElement(By.name("reg_email_confirmation__")).sendKeys("Raji@gmail.com");
+		driver.findElement(By.name("reg_passwd__")).sendKeys("Raji@gmail.com");
+		driver.findElement(By.name("birthday_day")).sendKeys("26");
+		driver.findElement(By.name("reg_email__")).sendKeys("Raji@gmail.com");
 	}
 	
 }
