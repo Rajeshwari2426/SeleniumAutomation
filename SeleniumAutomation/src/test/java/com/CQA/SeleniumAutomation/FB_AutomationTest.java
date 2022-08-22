f81fcdf59636906134fd73a85d4490ecbd34dd22
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-08-22 13:14:18 +0530

    [rajeshwari]adding css Selector

--- SeleniumAutomation/src/test/java/com/CQA/SeleniumAutomation/FB_AutomationTest.java
+++ SeleniumAutomation/src/test/java/com/CQA/SeleniumAutomation/FB_AutomationTest.java
@@ -1,4 +1,5 @@
 package com.CQA.SeleniumAutomation;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,30 +8,34 @@ import org.testng.annotations.Test;
 
 public class FB_AutomationTest {
 	public static WebDriver driver;
+
 	@BeforeTest
 	public void launchBrowser() {
 		System.setProperty("webdriver.chrome.driver",
-			"C:\\Users\\rajar\\eclipse-workspace\\SeleniumAutomation\\SeleniumAutomation\\Drivers\\chromedriver.exe");
-	driver = new ChromeDriver();
-	driver.get("https://www.facebook.com");		
+				"C:\\Users\\rajar\\eclipse-workspace\\SeleniumAutomation\\SeleniumAutomation\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
 	}
-	
+
 	@Test
-	public void logininTest() throws InterruptedException
-	{					
-		driver.findElement(By.name("email")).sendKeys("Raji@gmail.com");
-		driver.findElement(By.id("pass")).sendKeys("Raji");
+	public void logininTest() throws InterruptedException {
+		driver.findElement(By.name("email")).sendKeys("8790142483");
+																			          driver.findElement(By.id("pass")).sendKeys("jollysmile");
 		Thread.sleep(2000);
 		driver.findElement(By.name("login")).click();
-		
+		Thread.sleep(2000);
+		driver.close();
+
 	}
+
 	@Test
-	public void signupPageTest() throws InterruptedException
-	{
+	public void signupPageTest() throws InterruptedException {
 		driver.findElement(By.linkText("Create New Account")).click();
 		Thread.sleep(5000);
-		driver.findElement(By.name("firstname")).sendKeys("Raji");		
-		driver.findElement(By.name("lastname")).sendKeys("gandi");
+		driver.findElement(By.name("firstname")).sendKeys("Raji");
+		driver.findElement(By.name("firstname")).clear();
+		//driver.findElement(By.name("lastname")).sendKeys("gandi");
+		driver.findElement(By.cssSelector("input[name='lastname']")).sendKeys("gandi");
 		driver.findElement(By.name("reg_email__")).sendKeys("Rajibr2426@gmail.com");
 		driver.findElement(By.name("reg_email_confirmation__")).sendKeys("Rajibr2426@gmail.com");
 		driver.findElement(By.name("reg_passwd__")).sendKeys("Raji@gmail.com");
@@ -40,9 +45,11 @@ public class FB_AutomationTest {
 		driver.findElement(By.id("year")).sendKeys("1997");
 		driver.findElements(By.name("sex")).get(0).click();
 		driver.findElement(By.name("websubmit")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.id("jsc_c_5")).sendKeys("88299");//driver.findElement(By.name("login")).click();
-		
+		String text=driver.findElement(By.name("websubmit")).getAttribute("name");
+		System.out.println("text on button is:"+text);
+		Thread.sleep(3000);
+		driver.close();
+
 	}
-	
+
 }
--- SeleniumAutomation/src/test/java/com/CQA/SeleniumAutomation/LaunchBrowserTest.java
+++ SeleniumAutomation/src/test/java/com/CQA/SeleniumAutomation/LaunchBrowserTest.java
@@ -9,37 +9,39 @@ import org.testng.annotations.Test;
 
 public class LaunchBrowserTest {
 	public static WebDriver driver;
+
 	@Test
 	public void launchBrowser() throws InterruptedException {
-		Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
-		System.out.print("Select a browser:1.Chrome 2.Egde ");  
-		String str= sc.nextLine();              //reads string  
-		System.out.print("You have entered: "+str); 
-		
-		switch(str) {
-		case "1":System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\rajar\\eclipse-workspace\\SeleniumAutomation\\SeleniumAutomation\\Drivers\\chromedriver.exe");
-		       driver = new ChromeDriver();				
-		break;
+		Scanner sc = new Scanner(System.in); // System.in is a standard input stream
+		System.out.print("Select a browser:1.Chrome 2.Egde ");
+		String str = sc.nextLine(); // reads string
+		System.out.print("You have entered: " + str);
+
+		switch (str) {
+		case "1":
+			System.setProperty("webdriver.chrome.driver",
+					"C:\\Users\\rajar\\eclipse-workspace\\SeleniumAutomation\\SeleniumAutomation\\Drivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
 		case "2":
-					System.setProperty("webdriver.edge.driver",
-							"C:\\Users\\rajar\\eclipse-workspace\\SeleniumAutomation\\SeleniumAutomation\\Drivers\\msedgedriver.exe");
-					 driver = new EdgeDriver();					
-					
-		break;
+			System.setProperty("webdriver.edge.driver",
+					"C:\\Users\\rajar\\eclipse-workspace\\SeleniumAutomation\\SeleniumAutomation\\Drivers\\msedgedriver.exe");
+			driver = new EdgeDriver();
+
+			break;
 		}
 		driver.get("https://www.facebook.com");
 		driver.manage().window().maximize();
 		Thread.sleep(5000);
-//		String title = driver.getTitle();
-//		System.out.println("tITLE:" + title);
-//		Thread.sleep(2000);
-//		String currentUrl = driver.getCurrentUrl();
-//		System.out.println("URL:" + currentUrl);
-//		Thread.sleep(2000);
-//		String pageSource = driver.getPageSource();
-//		System.out.println("Source code:" + pageSource);
-//		Thread.sleep(2000);
+		String title = driver.getTitle();
+		System.out.println("tITLE:" + title);
+		Thread.sleep(2000);
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println("URL:" + currentUrl);
+		Thread.sleep(2000);
+		String pageSource = driver.getPageSource();
+		System.out.println("Source code:" + pageSource);
+		Thread.sleep(2000);
 		driver.close();
 	}
 
