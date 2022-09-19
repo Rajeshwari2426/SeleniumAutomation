1060e9505d5419f6b024a6c5df3f707a7513c046
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-09-19 09:39:20 +0530

    [Rajeshwari] adding POM login page

--- SeleniumAutomation/src/test/java/com/CQA/SeleniumAutomation/TestngTest.java
+++ SeleniumAutomation/src/test/java/com/CQA/SeleniumAutomation/TestngTest.java
@@ -46,5 +46,17 @@ public class TestngTest {
 		 public void afterSuite() {
 		 Reporter.log("afterSuite", true);
 		}
-
+		@Test(priority=0)
+		public void test1() {
+			Reporter.log("test1",true);
+		}
+		
+		@Test(groups= {"module1"})
+		public void test2() {
+			Reporter.log("test2",true);
+		}
+		@Test(invocationCount=2)
+		public void test3() {
+			Reporter.log("test3",true);
+		}
 }
